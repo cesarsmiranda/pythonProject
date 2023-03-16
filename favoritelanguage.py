d6e8cfd3b59009330da191b59fd7785c867a7802
cesarsmiranda <54806348+cesarsmiranda@users.noreply.github.com>
2023-03-15 21:41:44 -0300

    new

---
+++ favoritelanguage.py
@@ -0,0 +1,8 @@
+favorite_language = {
+    'cesar': 'python',
+    'luis': 'java',
+    'cecila': 'c++',
+    'ju': 'c',
+}
+
+print("Minha favorita linguagem é: " + str(favorite_language['cecila'].title() + "."))
