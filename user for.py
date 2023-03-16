6082c6d8bc8d5a9abf273ffad1960c75a515e4bd
cesarsmiranda <54806348+cesarsmiranda@users.noreply.github.com>
2023-03-15 21:50:06 -0300

    new

---
+++ user for.py
@@ -0,0 +1,15 @@
+user_0 = {
+    'username': 'cesar',
+    'first name': 'Cesar',
+    'last name': 'miranda',
+    }
+
+for k, v in user_0.items():
+    print("\nKey: " + k)
+    print("\nValue: " + v)
+
+
+
+
+
+
