954da89997378b56a48e517ee0d4326e8bd9da48
cesarsmiranda <54806348+cesarsmiranda@users.noreply.github.com>
2023-03-22 21:45:40 -0300

    new

---
+++ parrot.py
@@ -0,0 +1,11 @@
+prompt = "\nTell me something, and I will repeat is back to you"
+prompt = "\nEnter 'quit' to end the program"
+
+
+message = ""
+while message != 'quit':
+    message = input(prompt)
+    print(message)
+
+
+
