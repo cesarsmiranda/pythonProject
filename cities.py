9b541fd98a3de00d91d258f03f7635fa4e0a314d
cesarsmiranda <54806348+cesarsmiranda@users.noreply.github.com>
2023-03-22 21:49:56 -0300

    new

---
+++ cities.py
@@ -0,0 +1,11 @@
+prompt = "\nPlease enter the name of a city you have visited"
+prompt += "\n(Enter 'quit' when you are finished.)"
+
+while True:
+    city = input(prompt)
+
+    if city == 'quit':
+        break
+    else:
+        print("I'd love to got to " + city.title() + "!")
+
