de4c726dfbf58c09d0883ab64a1e756def6007c7
cesarsmiranda <54806348+cesarsmiranda@users.noreply.github.com>
2023-04-23 19:41:26 -0300

    new

--- write_message.py
+++ write_message.py
@@ -1,6 +1,6 @@
 filename = 'programing.txt'
 
 with open(filename, 'w') as file_object:
-    file_object.write("I love programing python")
-
+    file_object.write("I love programing python.\n")
+    file_object.write("I like python.\n")
 
